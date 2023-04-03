f7b3ae67f4921172a4b194bdb6109b75fb774de4
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-04-03 13:03:45 -0700

    Finished weekly challenge 2.

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet02
     {
+        public double CountofPositiveOddsBelowNumber { get; set; }
+
         public bool CharacterIsALetter(char c)
         {
             return char.IsLetter(c);
@@ -22,42 +24,101 @@ namespace ChallengesWithTestsMark8
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            return numbers.Where(number => number % 2 == 0).Sum();
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return false;
+            }
+            return numbers.Sum() % 2 != 0;
         }
+
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+
+            }
         }
     }
 }
+
+
+
+
+
+
---
+++ ChallengesWithTestsMark8/ChallengesSet02Helpers.cs
@@ -0,0 +1,16 @@
+﻿internal static class ChallengesSet02Helpers
+{
+
+
+    public static long CountOfPositiveOddsBelowNumber(int expected, int number)
+    {
+        if (number <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return number / 2;
+        }
+    }
+}
